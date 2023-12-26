1cbe8d64116b9ef7f1d93f668e5a34e0e6385e41
mdasifmunshe <asif35-2872@diu.edu.bd>
2023-12-27 01:45:40 +0600

    setup shadcn component library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from '@/components/ui/button'
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <div>Hello World!</div>
-    </main>
+    <div className="flex min-h-screen flex-col items-center p-24">
+      <Button variant="default">Click me</Button>
+    </div>
   )
 }
