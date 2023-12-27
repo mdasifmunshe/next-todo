cfbd19dc2917eaafbb5f5ca1e7d5d9d2dfb0fa63
mdasifmunshe <asif35-2872@diu.edu.bd>
2023-12-27 20:18:41 +0600

    setup Prisma and integrated with TRPC

--- src/server/routers/todo.ts
+++ src/server/routers/todo.ts
@@ -1,7 +1,30 @@
 import { publicProcedure, router } from '../trpc'
 import { TRPCError } from '@trpc/server'
+import { Prisma } from '@prisma/client'
+import { prisma } from '../prisma'
 import { z } from 'zod'
 
+const defaultTodoSelect = {
+  id: true,
+  title: true,
+  description: true,
+  createdAt: true,
+  updatedAt: true,
+} satisfies Prisma.TodoSelect
+
 export const todoRouter = router({
-  getTodos: publicProcedure.query(async () => 'GET All Todos'),
+  getAll: publicProcedure.query(async () => {
+    const todo = await prisma.todo.findMany({
+      select: defaultTodoSelect,
+    })
+
+    if (!todo) {
+      throw new TRPCError({
+        code: 'NOT_FOUND',
+        message: `No todo`,
+      })
+    }
+
+    return todo
+  }),
 })
