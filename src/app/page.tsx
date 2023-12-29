ddf5d9ecc39f03a7eb40ac88f31c8a5f270ccede
mdasifmunshe <asif35-2872@diu.edu.bd>
2023-12-29 11:14:26 +0600

    added SubHeader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import { Button } from '@/components/ui/button'
+import SubHeader from '@/components/SubHeader/SubHeader'
+import ListTodos from './list-todos'
 
 export default function HomePage() {
   return (
-    <div className="flex min-h-screen flex-col items-center bg-[#DAE0E6] p-24">
-      <Button variant="default">Click me</Button>
+    <div className="min-h-screen bg-[#DAE0E6] pt-12">
+      <div className="z-[3]">
+        <div className="box-border flex max-w-full flex-row justify-center px-6 py-5">
+          <div className="flex w-full flex-col items-center justify-center gap-4">
+            <SubHeader />
+            <div className="grid w-full grid-cols-1 place-items-center gap-[10px] xl:grid-cols-2 xl:gap-4">
+              <ListTodos />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
